539bdcaf7e4c0a097f47e354010ce6fdd3c7e7bb
Kerim Hadri <kerim.hadri@projekt8.net>
2026-01-26 19:05:21 +0100

    chore: install and configure eslint

---
+++ eslint.config.ts
@@ -0,0 +1,33 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
+import { defineConfig } from 'eslint/config';
+import { includeIgnoreFile } from '@eslint/compat';
+import { fileURLToPath } from 'node:url';
+
+const gitignorePath = fileURLToPath(new URL('.gitignore', import.meta.url));
+
+export default defineConfig([
+  includeIgnoreFile(gitignorePath, 'Imported .gitignore patterns'),
+  {
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+    languageOptions: { globals: globals.browser },
+  },
+  tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  pluginReact.configs.flat['jsx-runtime'],
+  {
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+    rules: {
+      'no-empty-pattern': 'off',
+      'react/no-unescaped-entities': 'off',
+    },
+  },
+]);
