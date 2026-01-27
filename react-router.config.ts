9580e676648670f39b7346c2bea44e57aae3b7c5
Kerim Hadri <kerim.hadri@projekt8.net>
2026-01-27 19:08:32 +0100

    fix: deployment base path

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,10 +1,12 @@
-import type { Config } from "@react-router/dev/config";
+import type { Config } from '@react-router/dev/config';
 
 export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
+  basename: process.env.BASE_PATH ?? '/',
+  routeDiscovery: { mode: 'initial' },
   ssr: true,
   prerender() {
-    return ["/", "/about"];
+    return ['/', '/about'];
   },
 } satisfies Config;
--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  base: process.env.BASE_PATH || '/',
+  base: process.env.BASE_PATH ?? '/',
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
 });
