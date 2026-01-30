fae67dc29e8876a8bf2f6e95092b5ab49c507b1a
Kerim Hadri <kerim.hadri@projekt8.net>
2026-01-30 16:39:23 +0100

    chore: install and configure vite svgr plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,9 @@ import { reactRouter } from '@react-router/dev/vite';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
   base: process.env.BASE_PATH ?? '/',
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  plugins: [tailwindcss(), reactRouter(), tsconfigPaths(), svgr()],
 });
